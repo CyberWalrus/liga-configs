202f6d12f4dc52a27ca115d4dfb8c4bbddab67af
Andrey Pakhomov <stonewalrusfan@gmail.com>
2022-08-02 23:00:05 +0000

    add cli

---
+++ commands/index.js
@@ -0,0 +1,43 @@
+const { Command } = require('commander');
+
+const printError = (err) => {
+	if (!err) {
+		console.trace('Unknown error');
+		return;
+	}
+
+	if (Array.isArray(err)) {
+		err.forEach(printError);
+		return;
+	}
+
+	if (err.message) {
+		console.error(err.message);
+	}
+
+	if (err.details) {
+		console.error(err.details);
+	}
+};
+
+const lint = async () => {
+	console.log('test');
+}
+
+(async () => {
+	try {
+
+		await new Command()
+			.addCommand(
+				new Command('lint')
+					.description('Отформатировать код, проверить js и css')
+					.action(lint),
+			)
+			.parseAsync(process.argv);
+			
+		process.exit(0);
+	} catch (err) {
+		printError(err);
+		process.exit(1);
+	}
+})();
