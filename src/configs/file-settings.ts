8015e7637b95da5d5908572854bf8769d5dc5d68
Andrey Pakhomov <stonewalrusfan@gmail.com>
2022-08-03 04:28:36 +0000

    changed fileSettings

--- src/configs/file-settings.ts
+++ src/configs/file-settings.ts
@@ -7,6 +7,7 @@ export type FileSettingsItem = {
 const fileSettings: FileSettingsItem[] = [
 	{ name: 'eslintrc.js', destination: '.eslintrc.js', overwrite: true },
 	{ name: 'eslintignore', destination: '.eslintignore' },
+	{ name: 'module-configs.json', destination: 'module-configs.json' },
 ];
 
 export default fileSettings;
